e295259b882c6962e22f746496d108b9fefab525
Lallith Prasath <117093226+Lallith-Prasath@users.noreply.github.com>
2024-04-19 14:39:56 +0530

    Update streamlit_app.py
    Added Info from Fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -20,11 +21,13 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
 
 if ingredients_list:
-
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+	st.subheader(fruit_chosen + 'Nutrition Information')
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+fruit_chosen)
+	fv_df = st.dataframe(data = fruityvice_respose.json(), use_container_width = True)
         
 
     #st.write(ingredients_string)
